8f09f1c9aae6ac0a9b9961573ccaa86c299fdccf
scottjue21 <scott.g.jue@gmail.com>
2025-02-12 14:51:23 -0800

    upload sql files

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -13,15 +13,16 @@ NOTE: If you are having issues with permissions. And you get error:
     1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
 5. Paste the following into `PSQL Tool`, (with the CORRECT file path)
 
-\copy company_dim FROM "C:\Users\sjue\Desktop\SQL Portfolio Projects\SQL_Project_Data_Job_Analysis\csv_files\company_dim.csv" WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy company_dim FROM 'C:\Users\sjue\Desktop\SQL Portfolio Projects\SQL_Project_Data_Job_Analysis\csv_files\company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_dim FROM "C:\Users\sjue\Desktop\SQL Portfolio Projects\SQL_Project_Data_Job_Analysis\csv_files\skills_dim.csv" WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy skills_dim FROM 'C:\Users\sjue\Desktop\SQL Portfolio Projects\SQL_Project_Data_Job_Analysis\csv_files\skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy job_postings_fact FROM "C:\Users\sjue\Desktop\SQL Portfolio Projects\SQL_Project_Data_Job_Analysis\csv_files\job_postings_fact.csv" WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy job_postings_fact FROM 'C:\Users\sjue\Desktop\SQL Portfolio Projects\SQL_Project_Data_Job_Analysis\csv_files\job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy skills_job_dim FROM "C:\Users\sjue\Desktop\SQL Portfolio Projects\SQL_Project_Data_Job_Analysis\csv_files\skills_job_dim.csv" WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy skills_job_dim FROM 'C:\Users\sjue\Desktop\SQL Portfolio Projects\SQL_Project_Data_Job_Analysis\csv_files\skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 */
+
 
 
 -- NOTE: This has been updated from the video to fix issues with encoding
